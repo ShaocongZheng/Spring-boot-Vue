2ff8d30bb2044509bee300cc1ffa9c337787fa82
zhengshaocong <512400820@qq.com>
2018-08-01 09:23:37 +0800

    add a home page

--- front_end/my_front_end/src/main.js
+++ front_end/my_front_end/src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import '../node_modules/element-ui/lib/theme-chalk/index.css'
 // import $ from 'jquery'
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
